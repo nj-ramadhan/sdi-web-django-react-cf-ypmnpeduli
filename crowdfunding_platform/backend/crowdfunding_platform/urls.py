e54428e7327addeae47eed9379b998da4afbf72b
Nur Jamiludin <nj.ramadhan.mechatronics@gmail.com>
2025-03-08 16:36:31 +0700

    update urls to handle ckeditor uploads

--- crowdfunding_platform/backend/crowdfunding_platform/urls.py
+++ crowdfunding_platform/backend/crowdfunding_platform/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from ckeditor_uploader import views as ckeditor_views
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from campaigns.views import CampaignViewSet
 from donations.views import DonationViewSet
-from campaigns.views import UpdateDonationView
-# from donations.views import UpdateDonationView
 
 router = DefaultRouter()
 router.register(r'campaigns', CampaignViewSet, basename='campaign')
@@ -17,7 +16,8 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/donations/', include('donations.urls')),
     
-    path('ckeditor/', include('ckeditor_uploader.urls')),    
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('ckeditor/upload/', ckeditor_views.upload, name='ckeditor_upload'),
     # path('api/auth/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
